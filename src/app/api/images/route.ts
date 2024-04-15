1394ed4289289682467f6d35c94ac74f6c0caa20
zorenal <zorenzal@gmail.com>
2024-04-15 14:26:06 -0700

    refactoring route

--- src/app/api/images/route.ts
+++ src/app/api/images/route.ts
@@ -1,4 +1,4 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 import { GetObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import { s3 } from '@/utils/s3'
@@ -10,11 +10,10 @@ import dotenv from 'dotenv';
 dotenv.config()
 
 type ResponseData = {
- url?: string,
- message?: string
+ url: string,
 }
 
-export async function GET(req: NextRequest,  res: NextResponse<ResponseData>) {
+export async function GET(): Promise<ResponseData> {
   try {
     const command = new GetObjectCommand({
       Bucket: process.env.AWS_BUCKET_NAME!,
@@ -23,7 +22,7 @@ export async function GET(req: NextRequest,  res: NextResponse<ResponseData>) {
     // for future: must change key to expressions of folder(map) and image
 
     const url = await getSignedUrl(s3, command,{ expiresIn: 3600 });
-    return new Response(url)
+    return new NextResponse(url)
   } catch (error) {
     console.error('Error fetching image from S3:', error);
     return new Response('Error fetching image from S3');
